53a793e32c4880f0ed0a8d662dbe9503a3377637
Kamoliddin Mirzaboyev <kamoliddinmirzaboyev05@gmail.com>
2025-01-05 18:31:42 +0500

    Upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const body = document.querySelector("body");
+const modeBtn = document.querySelector("#mode");
+const logo = document.querySelector(".logo");
+const boxes = document.querySelectorAll(".box-mode");
+const lighters = document.querySelectorAll(".lighter-type");
+const grayers = document.querySelectorAll(".grayer-type");
+var darkMode = false;
+modeBtn.addEventListener("click", () => {
+  if (darkMode == false) {
+    darkMode = true;
+    body.classList.add("bg-dark");
+    boxes.forEach((box) => {
+      box.classList.add("box-dark");
+    });
+    modeBtn.setAttribute("src", "img/sun.svg");
+    logo.setAttribute("src", "img/white-logo.svg");
+    lighters.forEach((lighter) => {
+      lighter.classList.add("lightertxt");
+      grayers.forEach((grayer) => {
+        grayer.classList.add("grayer-text");
+      });
+    });
+  } else {
+    darkMode = false;
+    body.classList.remove("bg-dark");
+    boxes.forEach((box) => {
+      box.classList.remove("box-dark");
+    });
+    modeBtn.setAttribute("src", "img/moon.svg");
+    logo.setAttribute("src", "img/logo.svg");
+    lighters.forEach((lighter) => {
+      lighter.classList.remove("lightertxt");
+    });
+    grayers.forEach((grayer)=>{
+      grayer.classList.remove("grayer-text")
+    })
+  }
+  lighters.forEach((lighter) => {
+    lighter.classList.remove("lighter-text");
+  });
+});
