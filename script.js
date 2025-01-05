1afdf5843bdeec9c57b80023c1d6b0dfcf746503
Kamoliddin Mirzaboyev <kamoliddinmirzaboyev05@gmail.com>
2025-01-05 18:56:37 +0500

    Update

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const logo = document.querySelector(".logo");
 const boxes = document.querySelectorAll(".box-mode");
 const lighters = document.querySelectorAll(".lighter-type");
 const grayers = document.querySelectorAll(".grayer-type");
+const grayerBtns = document.querySelectorAll(".grayer-btn");
 var darkMode = false;
 modeBtn.addEventListener("click", () => {
   if (darkMode == false) {
@@ -19,6 +20,9 @@ modeBtn.addEventListener("click", () => {
       grayers.forEach((grayer) => {
         grayer.classList.add("grayer-text");
       });
+      grayerBtns.forEach((grayerBtn) => {
+        grayerBtn.classList.add("grayerBtn");
+      });
     });
   } else {
     darkMode = false;
@@ -31,9 +35,12 @@ modeBtn.addEventListener("click", () => {
     lighters.forEach((lighter) => {
       lighter.classList.remove("lightertxt");
     });
-    grayers.forEach((grayer)=>{
-      grayer.classList.remove("grayer-text")
-    })
+    grayers.forEach((grayer) => {
+      grayer.classList.remove("grayer-text");
+    });
+    grayerBtns.forEach((grayerBtn) => {
+      grayerBtn.classList.remove("grayerBtn");
+    });
   }
   lighters.forEach((lighter) => {
     lighter.classList.remove("lighter-text");
